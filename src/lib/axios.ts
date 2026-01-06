bf08f7e0c11ad1852299f21bf2ea04e60963d0d0
Apanco117 <dperezapanco@gmail.com>
2026-01-06 20:22:58 +0000

    correccion de redireccion

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { toast } from 'sonner';
 
 //. ->  Crear instancia de axios
 //console.log('VITE_API_URL leída:', import.meta.env.VITE_API_URL);
@@ -17,33 +16,5 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response) {
-            if (error.response.status === 403) {
-                window.location.href = '/dashboard';
-                toast.warning("Acceso Restringido", {
-                    description: "No tienes permisos para ver esta sección. Redirigiendo...",
-                    duration: 4000,
-                });
-                return Promise.reject(error);
-            }
-
-            if (error.response.status === 401) {
-                localStorage.removeItem('AUTH_TOKEN');
-                toast.error("Sesión Expirada", {
-                    description: "Tu token de seguridad ha caducado. Por favor inicia sesión de nuevo.",
-                });
-                window.location.href = '/auth/login';
-                return Promise.reject(error);
-            }
-        }
-        
-        // Rechazamos la promesa para que React Query sepa que falló
-        return Promise.reject(error);
-    }
-);
 
 export default api
